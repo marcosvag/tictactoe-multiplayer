2e63aad80e3fcfff7493c26c6cc7fddb5efd98ca
marcosvag <101986630+marcosvag@users.noreply.github.com>
2022-08-20 01:56:13 -0300

    configured views, layouts, routes, sessions, passport, middleware and database connection

--- server.js
+++ server.js
@@ -1,36 +1,62 @@
 const express = require('express');
-const passport = require('passport-google-oauth20');
+const expressLayouts = require('express-ejs-layouts')
+const passport = require('passport');
 const session = require('express-session');
-const hbs = require('express-handlebars');
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv');
 const { Server } = require('socket.io');
 const mongoose = require('mongoose');
-const connectMongo = require('connect-mongo');
+const ConnectDatabase = require('./config/database');
+const MongoStore = require('connect-mongo')
 const http = require('http');
 const PORT = process.env.PORT || 333;
 
+// ENV config
+dotenv.config({ path: './config/.env' });
 
+// Passport config
+require('./config/passport')(passport);
+
+ConnectDatabase();
 
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(expressLayouts);
+app.set('layout', 'layouts/layout')
 
-const server = http.createServer(app)
-const io = new Server(server)
+const server = http.createServer(app);
 
-app.get('/', (request, responde) => {
-    responde.sendFile(__dirname + '/index.html')
-})
+// WebSocket
+const io = new Server(server);
 
 io.on('connection', (socket) => {
-    console.log('a user connected')
+    console.log('a user connected');
     socket.on('choice', (msg) => {
-        io.emit('choice', msg)
-        console.log(msg)
+        io.emit('choice', msg);
+        console.log(msg);
     })
     socket.on('disconnect', () => {
-        console.log('user disconnected')
+        console.log('user disconnected');
     })
-})
+});
 
-server.listen(PORT)
+// Sessions
+app.use(session({
+    secret: 'yesSir',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_STRING }),
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
+app.use('/game', require('./routes/play'));
+
+server.listen(PORT);
